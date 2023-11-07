30eb6b66b6eb2b29dad796e2c7007dc8b0d08e48
Yetity <hlonipoole692@gmail.com>
2023-11-07 07:05:33 +0200

    smooth image switch

--- script.js
+++ script.js
@@ -3,28 +3,40 @@ var merged = document.getElementById("merged"); // logo
 var state = false; // logo state
 
 class ImageShowcase {
-    /* Get the element's id by the given name and get their respective image links.*/
+    /* Get the element's id by the given name and get their respective image links. */
     constructor(name) {
         this.img = document.getElementById(name);
+        this.img.style.transition = "opacity 1s"; // Add a CSS transition for smooth opacity changes
         let imageLink = `${base}${name}/`;
         this.images = [`${imageLink}git.png`, `${imageLink}discord.png`];
     }
 }
 
-/* Sort 2 images with classes.*/
+/* Sort 2 images with classes. */
 var yet = new ImageShowcase("yet");
 var flo = new ImageShowcase("flo");
 
 var currentImageIndex = 0; // image index for image switching
-const dummyArr = ["", ""]; // length of user images array
+const dummyArr = new Array(yet.images.length).fill(""); // length of user images array. should match yet since yet = flo
 
-function changeImage() { // Change the image for both yet and flo if there are multiple images.
-    yet.img.src = yet.images[currentImageIndex];
-    flo.img.src = flo.images[currentImageIndex];
+function changeImage() {
+    // Smoothly fade in the new image by changing opacity
+    yet.img.style.opacity = 0;
+    flo.img.style.opacity = 0;
+
+    setTimeout(() => {
+        yet.img.src = yet.images[currentImageIndex];
+        flo.img.src = flo.images[currentImageIndex];
+
+        // Smoothly fade in the new image by changing opacity
+        yet.img.style.opacity = 1;
+        flo.img.style.opacity = 1;
+    }, 200); // Wait for 200ms before changing the source
     currentImageIndex = (currentImageIndex + 1) % dummyArr.length;
 }
 
-function toggleState() { // switch logo state
+function toggleState() {
+    // switch logo state
     state = !state;
     merged.classList.toggle("switch", state);
 }
